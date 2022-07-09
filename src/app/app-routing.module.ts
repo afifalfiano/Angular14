c3768ed4c6434315b6b5cb1daed0e3f7f20ebad1
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-07-09 18:57:35 +0700

    fix: error import home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './public/home/home.component';
+// import { HomeComponent } from './public/home/home.component';
 
 const routes: Routes = [
-  {path:'home', component:HomeComponent}
+  {path: 'home', loadChildren: () => import('./public/public.module').then(m => m.PublicModule)}
 ];
 
 @NgModule({
